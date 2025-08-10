e58f71f52651f3c67d75abd32a7aee231c32a88c
Random-guy1374 <023243.ac.th@gmail.com>
2025-08-10 05:02:08 +0000

    Your message here

---
+++ u01_pythonbasics/Creative Repetition.py
@@ -0,0 +1,3 @@
+phrase1 = "Let's go! "
+phrase2 = "We can do this"
+print(phrase1 * 3, phrase2)
---
+++ u01_pythonbasics/String Combination.py
@@ -0,0 +1,6 @@
+name = "Alice"
+hobby = "reading"
+# first way
+print(name,"loves",hobby,"in her free time.")
+# second way
+print(f"{name} loves {hobby} in her free time.")
