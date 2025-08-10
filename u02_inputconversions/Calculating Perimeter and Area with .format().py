f576e50d5996d2f8bb160638c81b401cdf9201c7
Random-guy1374 <023243.ac.th@gmail.com>
2025-08-10 08:35:07 +0000

    Add perimeter/area and temperature conversion scripts using .format() in u02

---
+++ u02_inputconversions/Calculating Perimeter and Area with .format().py
@@ -0,0 +1,5 @@
+l = float(input("Enter a length of a rectangle :"))
+w = float(input("Enter a width of a rectangle :"))
+perimeter = 2* (l + w)
+area = l*w
+print(f"Perimeter:{perimeter},Area:{area}")
---
+++ u02_inputconversions/Temperature Conversion with .format().py
@@ -0,0 +1,3 @@
+c = float(input("Celsius :"))
+f = (c*9/5) + 32
+print("{}°C is equal to {}°F.".format(c,f))
