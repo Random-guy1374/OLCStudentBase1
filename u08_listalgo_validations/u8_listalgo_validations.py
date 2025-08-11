5cd179bda452725ebfa848418712f4badf1c5069
Random-guy1374 <023243.ac.th@gmail.com>
2025-08-11 01:20:47 +0000

    Rename listfunctions scripts for clarity and update listalgo validations

---
+++ u08_listalgo_validations/u8_listalgo_validations.py
@@ -0,0 +1,38 @@
+# Part 2. IN-CLASS Practice Exercises
+
+# Exercise 8: Finding the Smallest Without Built-In Functions
+# Write a program to find the smallest number in a list without 
+# using min().
+numbers = [1, 2, 2, 3, 4, 4, 5, 5, 6, 6]
+
+
+
+
+
+#------------------------------------------------------------
+# Exercise 9: Counting Occurrences of a Value
+# Write a program to count how many times a specific number 
+# appears in a list.
+# Example: Input = [1, 2, 2, 3], Check for 2.
+numbers = [1, 2, 2, 3, 2, 4, 2, 5, 2]
+
+
+
+
+
+#------------------------------------------------------------
+# Exercise 10: Removing Duplicates from a List
+# Write a program to remove duplicate values from a list.
+# Example: Input = [1, 2, 2, 3, 4, 4], Output = [1, 2, 3, 4].
+numbers = [1, 2, 2, 3, 4, 4, 5, 5, 6, 6]
+
+
+
+
+
+#------------------------------------------------------------
+# Exercise 11: Ensuring All Numbers Are Positive
+# Write a program to check that all numbers in a list are 
+# positive. If any number is negative, remove it and print the 
+# updated list.
+numbers = [10, -5, 20, -15, 30, -25]
