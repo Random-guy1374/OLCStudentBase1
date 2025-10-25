d279d32b9ab80d507f2fb103ea3d91773abe9a4b
Random-guy1374 <023243.ac.th@gmail.com>
2025-10-25 06:31:10 +0000

    Debugging/Asgmt_2018.py

--- Debugging/Asgmt_2018.py
+++ Debugging/Asgmt_2018.py
@@ -18,13 +18,13 @@
 #         students = True
 
 students = False
-while students == False: #2. True to Flase
+while students == False: #2. cahnge True to Flase
     comp = int(input("Enter the Computing test score ")) #4. add int
     math = int(input("Enter the Mathematics test score ")) #1 add "
     joint_score = comp + math #3. comp + comp to comp + math
     if comp == 100 and math == 100: #7. > to ==
         print("Student is awarded a gold award")
-    elif(comp > 100 and math > 100) or joint_score >= 180: #5. add () #6. >= to =
+    elif(comp > 100 or math > 100) and joint_score >= 180: #5. add () #8. >= to = #10 change and to or #11 change or to and
         print("Student is awarded a silver award")
     elif comp >= 75 and math >= 75:
         print("Student is awarded a bronze award")
@@ -34,4 +34,4 @@ while students == False: #2. True to Flase
     if more_scores == 'N': #6. More_scores to more_scores
         students = True
     else:
-        students = False #7. True to False
+        students = False #9. True to False
