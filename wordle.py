579c94b42bd33ca2438bb023613c2dcd0a2019c0
Random-guy1374 <023243.ac.th@gmail.com>
2025-11-16 03:56:53 +0000

    wordle.py

--- wordle.py
+++ wordle.py
@@ -1,6 +1,9 @@
+#1)
 with open("FiveLetterWords.txt","r") as f:
     contents = f.read().split(",")
     # print(contents)
+
+#2?
 empty = []
 vowels = ["a","e","i","o","u"]
 letters = []
@@ -12,6 +15,40 @@ for word in contents:
     if vowel >= 4 :
         empty += [word]
 print(empty)
+
+#3)
+vowel1 = [] 
+vowel2 = []
+vowel3 = []
+vowel4 = []
+vowel5 = []
+letters = []
+for word in contents:
+    vowel = 0 # for every word, reset vowel count
+    for v in vowels:
+        if v in word:
+            vowel += 1 
+    if vowel == 1:
+        vowel1 += [word]
+    if vowel == 2:
+        vowel2 += [word]
+    if vowel == 3:
+        vowel3 += [word]
+    if vowel == 4:
+        vowel4 += [word]
+    if vowel == 5:
+        vowel5 += [word]
+#4)
+# print("words with 1 vowel:",vowel1)
+# print("\n")
+# print("words with 2 vowel:",vowel2)
+# print("\n")
+# print("words with 3 vowel:",vowel3)
+# print("\n")
+print("words with 4 vowel:",vowel4)
+print("\n")
+print("words with 5 vowel:",vowel5)
+
 # user_word = input("Enter a 5 letters word: ")
 # while True:
 #     if len(user_word) != 5:
