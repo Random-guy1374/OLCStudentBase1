78acf60f595c35cc3de57e6ee3cea481691dd090
Random-guy1374 <023243.ac.th@gmail.com>
2026-07-01 10:30:58 +0000

    01-07-2026

---  Refinement Revision Challenge/VerifySG.py
+++  Refinement Revision Challenge/VerifySG.py
@@ -7,9 +7,17 @@
 #         print("Welcome to Singapore!")
 
 ID = ""
+count = 0
 for i in range(5):
-    ID = input("Enter ID: ")
-    if ID[0] == "S" or ID[0] =="T":
+    while True:
+        ID = input("Enter ID: ")
+        if len(ID) != 9:
+            print("Invalid ID. Please enter a valid ID.")
+        else:
+            break
+    if ID[0].upper() in ["S","T"]:
         print("Welcome home!")
+        count += 1
     else:
         print("Welcome to Singapore!")
+print("There are",count,"Singaporeans.")
