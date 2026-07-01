5dd46c7f76975a2e9a5f778e2d6bf126a5f46e58
Random-guy1374 <023243.ac.th@gmail.com>
2026-07-01 10:23:28 +0000

    1/7/2026

---
+++  Refinement Revision Challenge/VerifySG.py
@@ -0,0 +1,15 @@
+# ID = ""
+# for i in range(2):
+#     ID = input("Enter ID: ")
+#     if ID[0] == "S":
+#         print("Welcome home!")
+#     else:
+#         print("Welcome to Singapore!")
+
+ID = ""
+for i in range(5):
+    ID = input("Enter ID: ")
+    if ID[0] == "S" or ID[0] =="T":
+        print("Welcome home!")
+    else:
+        print("Welcome to Singapore!")
